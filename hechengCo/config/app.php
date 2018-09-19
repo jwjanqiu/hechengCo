58bb20cd568d7aaa4e80f375d0d6f1c94181a58c
jwjanqiu <jwjanqiu@outlook.com>
2018-09-19 17:44:48 +0800

    1.添加日志记录

--- hechengCo/config/app.php
+++ hechengCo/config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'PRC',
 
     /*
     |--------------------------------------------------------------------------
@@ -146,6 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
 
         /*
          * Package Service Providers...
